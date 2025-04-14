4cb5f62df49bc2ea0c493d04e2fd5352d1b30df3
extremecoder <trainings.abhishek@gmail.com>
2025-04-14 17:50:38 +0530

    Remove setup.py and clean up microservice generation command in e2e pipeline template.

--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup
-
-# This file is maintained for compatibility with older toolchains
-# The actual package configuration is in pyproject.toml
-setup(
-    include_package_data=True,
-) 
