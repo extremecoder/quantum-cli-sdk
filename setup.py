83588fe8866f05dee793639115a0d187ea0546ad
extremecoder <trainings.abhishek@gmail.com>
2025-04-14 16:25:19 +0530

    Fix microservice generation in GitHub Actions by ensuring template files are included in the package with MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+# This file is maintained for compatibility with older toolchains
+# The actual package configuration is in pyproject.toml
+setup(
+    include_package_data=True,
+) 
